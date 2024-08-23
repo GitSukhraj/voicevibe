ee6b7b13377d12237cdc478c28efdbc9b049a31c
GitSukhraj <79049940+SukhrajProgrammer@users.noreply.github.com>
2024-08-23 10:17:32 +0200

    update

--- js/t_detail.js
+++ js/t_detail.js
@@ -1,11 +1,15 @@
 var basicInfo = document.getElementById("basic-info");
 var details = document.getElementById("details");
+var basicText = document.getElementById("basic-content");
+var detailText = document.getElementById("detail-content");
 
 basicInfo.onclick = function () {
     basicInfo.style.border = "1.5px solid rgb(187, 187, 187)";
     basicInfo.style.borderBottom = "none";
     details.style.border = "none";
     details.style.borderBottom = "1.5px solid rgb(187, 187, 187)";
+    detailText.style.display = "none";
+    basicText.style.display = "block";
 };
 
 details.onclick = function () {
@@ -13,4 +17,6 @@ details.onclick = function () {
     details.style.borderBottom = "none";
     basicInfo.style.border = "none";
     basicInfo.style.borderBottom = "1.5px solid rgb(187, 187, 187)";
+    basicText.style.display = "none";
+    detailText.style.display = "block";
 };

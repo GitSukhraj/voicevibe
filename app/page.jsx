2c7f14efc1d50779b652f05f028a9be37c3f2895
GitSukhraj <79049940+SukhrajProgrammer@users.noreply.github.com>
2024-09-27 16:48:34 +0200

    update

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">Discover and share
+                <br className="max-md:hidden" />
+                <span className="blue_gradient text-center">Different topics</span>
+            </h1>
+            <p className="desc text-center">
+                Share your thoughts on different topics, create polls and vote.
+                Find the most trending topics right now. Know what the world is thinking.
+            </p>
+
+
+        </section>
+    )
+}
+
+export default Home;

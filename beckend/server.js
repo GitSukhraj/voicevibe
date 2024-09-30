23d92898448e7b1b3bc33aea3f3d57185f6e02b3
GitSukhraj <79049940+SukhrajProgrammer@users.noreply.github.com>
2024-09-30 21:16:23 +0200

    update

--- beckend/server.js
+++ beckend/server.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const cookieparser = require('cookie-parser');
 
 const app = express();
 
-const port = 3000;
+app.use(express.json());
+app.use(cors());
+app.use(cookieparser);
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.status(500).send("hello world!");
